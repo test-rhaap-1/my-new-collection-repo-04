4aca7f2d7f19711dce9039e7da0440f5e3f6a9ef
Scaffolder <scaffolder@backstage.io>
2024-11-15 09:05:49 +0000

    initial commit

---
+++ tests/unit/test_basic.py
@@ -0,0 +1,6 @@
+"""Unit tests for my_new_collection_namespace_04.my_new_collection_name_04."""
+
+
+def test_basic() -> None:
+    """Dummy unit test that always passes."""
+    assert bool(1) is True
